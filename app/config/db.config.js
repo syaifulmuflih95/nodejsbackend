f5a189a617f5aa28dc1556b37ae9e2e75dc7e52b
Angga Yulfardhi <momochan_suki@yahoo.com>
2022-06-21 10:09:46 +0700

    Test

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,32 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "crud",
-    dialect: "mysql",
-    pool:{
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "crud",
+//     dialect: "mysql",
+//     pool:{
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
 
-};
+// };
+
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+
+db.connect(function(error){
+    if(error){
+        console.error(error);
+    } else {
+        console.info("Connected to Database");
+    }
+});
+
+module.exports = db;
